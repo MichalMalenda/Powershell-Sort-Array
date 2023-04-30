030d3cc7177b3399e25dbb203ddc8053bcf7ec22
Michał Malenda <michalmalenda@gmail.com>
2023-04-30 22:52:30 +0200

    init
    init with Bubble sort

---
+++ Sort-Array.psm1
@@ -0,0 +1,20 @@
+﻿Function Sort-Array{
+    param(
+        #[Parameter(Mandatory=$true)]
+        #[string]$Algorithm,
+        [Parameter(Mandatory=$true)]
+        [array]$Array
+    )
+    Write-Output "IN - $Array"
+    for($i=0;$i -lt $Array.Count; $i++){
+        for($j=0;$j -lt $Array.Count-$i-1; $j++){
+            if($Array[$j] -gt $Array[$j+1]){
+                $swap=$Array[$j]
+                $Array[$j]=$Array[$j+1]
+                $Array[$j+1]=$swap
+                Write-Output "Step $i - $Array"
+            }
+        }
+    }
+    Write-Output "OUT - $Array"
+}
